a09e47158380596b55cb67cb4b210be87b0d8c62
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-02-11 17:15:28 -0300

    Adicionando anotações dinamicamente

--- index.php
+++ index.php
@@ -6,9 +6,26 @@
 </head>
 <body>
 
+<div class="container-anotacoes">
+
+
+</div><!--container-anotacoes-->
+
+
 <div class="btn-add">+</div>
 
 
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+
+<script>
+    $(function(){
+        $('.btn-add').click(function(){
+            var el = '<div class="anotacao-single"><textarea placeholder="Sua nova anotação..."></textarea></div>' ;
+            $('.container-anotacoes').append(el);
+        })
+    })
+</script>
+
 </body>
 </html>
