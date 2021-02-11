fb5abd412511080b06967937b9dd14b79f308032
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-02-11 16:54:54 -0300

    Criação e estilização do botão de adicionar

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Anotações</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<div class="btn-add">+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+</body>
+</html>
