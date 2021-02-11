0d92983fe6e89108d0ca4f1308f26d029d82bce2
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-02-11 17:26:42 -0300

    Adicionando horário à anotação

--- index.php
+++ index.php
@@ -23,6 +23,17 @@
         $('.btn-add').click(function(){
             var el = '<div class="anotacao-single"><textarea placeholder="Sua nova anotação..."></textarea></div>' ;
             $('.container-anotacoes').append(el);
+
+            var textarea = $('.anotacao-single').last().find('textarea');
+
+            var date = new Date();
+            var h = date.getHours();
+            var min = date.getMinutes();
+
+            var finalTime = h+':'+min;
+
+            textarea.html(finalTime);
+
         })
     })
 </script>
